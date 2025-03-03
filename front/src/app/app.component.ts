3734bea26a0711867b553ee428e13bddf8468ec7
sahnas <sahnas@hotmail.fr>
2025-03-03 16:41:09 +0100

    feat(front): add routerLink to logo for navigation

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,20 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ToastComponent } from './shared/toast/toast.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ToastComponent],
+  imports: [RouterOutlet, RouterLink, ToastComponent],
   template: `
     <div class="min-h-screen bg-gray-50">
       <header class="bg-white shadow-sm">
         <div class="max-w-6xl mx-auto px-4 py-4">
-          <img
-            src="https://www.envoituresimone.com/packs/media/images/logo-799cc6b9049e7e56d573120ec02c5eb5.svg"
-            alt="En Voiture Simone"
-            class="h-8"
-          />
+          <a routerLink="/exams">
+            <img
+              src="https://www.envoituresimone.com/packs/media/images/logo-799cc6b9049e7e56d573120ec02c5eb5.svg"
+              alt="En Voiture Simone"
+              class="h-8"
+            />
+          </a>
         </div>
       </header>
 
